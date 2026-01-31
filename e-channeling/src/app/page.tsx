ea3dd63bedd2b0f6bef5738db4f96dd1e7591ac5
Chamindu-Dharmawickrema <chamindudharmawickrema@gmail.com>
2026-01-31 13:02:59 +0530

    Update page.tsx

--- e-channeling/src/app/page.tsx
+++ e-channeling/src/app/page.tsx
@@ -4,22 +4,10 @@ import FeaturedServices from "../components/home/FeaturedServices";
 import MembershipSection from "../components/home/MembershipSection";
 import Welcome from "@/components/home/Welcome";
 import QuickAccess from "../components/home/QuickAccess";
-import Snowfall from "react-snowfall";
 
 export default function Home() {
     return (
         <main className="min-h-screen px-5 sm:px-15 pt-8">
-            <Snowfall
-                color="white"
-                snowflakeCount={200}
-                style={{
-                    position: "fixed",
-                    width: "100vw",
-                    height: "100vh",
-                    zIndex: 9999,
-                    pointerEvents: "none",
-                }}
-            />
             {/* Landing Page */}
             <HeroSection />
             <Welcome />
